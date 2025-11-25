77d0cf69bb8c8686e5af188ab537f81095b13c34
Stephen Ojogbede <stephen.ojogbede@bbc.co.uk>
2025-11-25 14:48:40 +0000

    Setup PokeAPI service and fetch initial list

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react";
+
 function Body() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		const fetchPokemon = async () => {
+			try {
+				const reponse = await fetch("https://pokeapi.co/api/v2/pokemon/");
+				const result = await reponse.json();
+				setData(result.results);
+				console.log(result.results); //data will update on next render so can't log it here
+			} catch (error) {
+				console.error("Error fetching data:", error);
+			}
+		};
+		fetchPokemon();
+	}, []);
+
 	return (
 		<>
-			<h1>Body</h1>
+			<ul>
+				{data.map((pokemon, index) => (
+					<li key={index}>{pokemon.name}</li>
+				))}
+			</ul>
 		</>
 	);
 }
