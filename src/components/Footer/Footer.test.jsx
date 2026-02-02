39b306c03d3b2b838e732cfee8f9a62f4b4e8d14
Stephen Ojogbede <stephen.ojogbede@bbc.co.uk>
2026-02-02 16:34:20 +0000

    Unit tests for footer

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -1,11 +1,24 @@
 import Footer from "./Footer";
-import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom/vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 
 describe("Footer", () => {
-	it("dummy test that always passes", () => {
-		expect(true).toBe(true);
+	it("renders the my name name", () => {
+		render(<Footer />);
+		expect(screen.getByText(/Stephen Ojogbede/i)).toBeInTheDocument();
+	});
+
+	it("renders the GitHub link with correct href", () => {
+		render(<Footer />);
+		const link = screen.getByRole("link", { name: /view code on github/i });
+		expect(link).toBeInTheDocument();
+		expect(link).toHaveAttribute("href", "https://github.com/ojogbs01/pokemon-app");
+		expect(link).toHaveAttribute("target", "_blank");
+	});
+
+	it("renders the copyright", () => {
+		render(<Footer />);
+		expect(screen.getByText(/© 2025 Pokédex. All rights reserved./i)).toBeInTheDocument();
 	});
 });
