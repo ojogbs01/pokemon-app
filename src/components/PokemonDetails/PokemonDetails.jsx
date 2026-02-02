12f7a4a702eab4039cde2f86872aff40e99598c4
Stephen Ojogbede <stephen.ojogbede@bbc.co.uk>
2026-02-02 12:08:38 +0000

    Setup testing environment

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -2,27 +2,6 @@ import styles from "./PokemonDetails.module.css";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-const typeStyles = {
-	normal: { background: "#A8A878", color: "#1C1C1C" },
-	fire: { background: "#F08030", color: "#1C1C1C" },
-	water: { background: "#6890F0", color: "#0E2233" },
-	electric: { background: "#F8D030", color: "#1C1C1C" },
-	grass: { background: "#78C850", color: "#0E2216" },
-	ice: { background: "#98D8D8", color: "#0E2222" },
-	fighting: { background: "#C03028", color: "#FFFFFF" },
-	poison: { background: "#A040A0", color: "#FFFFFF" },
-	ground: { background: "#E0C068", color: "#1C1C1C" },
-	flying: { background: "#A890F0", color: "#0E2030" },
-	psychic: { background: "#F85888", color: "#1C1C1C" },
-	bug: { background: "#A8B820", color: "#1C1C1C" },
-	rock: { background: "#B8A038", color: "#1C1C1C" },
-	ghost: { background: "#705898", color: "#FFFFFF" },
-	dragon: { background: "#7038F8", color: "#FFFFFF" },
-	dark: { background: "#705848", color: "#FFFFFF" },
-	steel: { background: "#B8B8D0", color: "#0E2030" },
-	fairy: { background: "#EE99AC", color: "#1C1C1C" },
-};
-
 const typeColors = {
 	normal: "#A8A77A",
 	fire: "#EE8130",
