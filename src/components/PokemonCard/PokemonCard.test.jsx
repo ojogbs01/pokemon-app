035aaf7b1abb6237ba526c07aaef9f79f4e69d69
Stephen Ojogbede <stephen.ojogbede@bbc.co.uk>
2026-02-03 12:06:28 +0000

    Unit tests for pokemon card

--- src/components/PokemonCard/PokemonCard.test.jsx
+++ src/components/PokemonCard/PokemonCard.test.jsx
@@ -1,11 +1,37 @@
 import PokemonCard from "./PokemonCard";
-import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom/vitest";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 
 describe("PokemonCard", () => {
-	it("dummy test that always passes", () => {
-		expect(true).toBe(true);
+	it("renders the pokemon name", () => {
+		render(<PokemonCard name="Bulbasaur" id={1} types={["Grass, Poison"]} />);
+		expect(screen.getByText("Bulbasaur")).toBeInTheDocument();
+	});
+
+	it("renders the pokemon ID", () => {
+		render(<PokemonCard name="Bulbasaur" id={1} types={["Grass, Poison"]} />);
+		expect(screen.getByText("#1")).toBeInTheDocument();
+	});
+
+	it("renders the pokemon sprite image", () => {
+		render(<PokemonCard name="Bulbasaur" id={1} types={["Grass, Poison"]} />);
+		const img = screen.getByAltText("Pokemon Sprite");
+		expect(img).toBeInTheDocument();
+		expect(img).toHaveAttribute(
+			"src",
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+		);
+	});
+
+	it("renders the pokemon types", () => {
+		render(<PokemonCard name="Bulbasaur" id={1} types={["Grass, Poison"]} />);
+		expect(screen.getByText(/Grass/i)).toBeInTheDocument();
+		expect(screen.getByText(/Poison/i)).toBeInTheDocument();
+	});
+
+	it('shows "Loading..." if types prop is missing', () => {
+		render(<PokemonCard name="Bulbasaur" id={1} />);
+		expect(screen.getByText("Loading...")).toBeInTheDocument();
 	});
 });
